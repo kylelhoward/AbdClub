f576acf6cae3e22444a176dc5872d3d0aa202439
Kyle Howard <kylelhoward@gmail.com>
2026-08-29 15:57:27 -0500

    Add SpecialAnnouncments

---
+++ AbdClub/Migrations/20260829201550_AddSpecialAnnouncement.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace AbdClub.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSpecialAnnouncement : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SpecialAnnouncements",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: true),
+                    OriginalFileName = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    ContentType = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    FileData = table.Column<byte[]>(type: "bytea", nullable: false),
+                    IsPublished = table.Column<bool>(type: "boolean", nullable: false),
+                    UploadedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UploadedByOfficerAccountId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SpecialAnnouncements", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SpecialAnnouncements_OfficerAccounts_UploadedByOfficerAccou~",
+                        column: x => x.UploadedByOfficerAccountId,
+                        principalTable: "OfficerAccounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SpecialAnnouncements_UploadedByOfficerAccountId",
+                table: "SpecialAnnouncements",
+                column: "UploadedByOfficerAccountId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SpecialAnnouncements");
+        }
+    }
+}
