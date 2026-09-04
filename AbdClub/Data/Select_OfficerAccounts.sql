2b972c6e4f5feb11a943fcf4e72b2d8e93b756b2
Kyle Howard <kylelhoward@gmail.com>
2026-09-04 16:06:11 -0500

    Updated Select_OfficerAccounts.sql

--- AbdClub/Data/Select_OfficerAccounts.sql
+++ AbdClub/Data/Select_OfficerAccounts.sql
@@ -1,10 +1,33 @@
-SELECT "Id",
-       "Email",
-       "GoogleSubId",
-       "AccessLevel",
-       "OfficerTitle",
-       "IsEnabled",
-       "MemberId",
-       "CreatedAt"
-FROM public."OfficerAccounts"
+SELECT o."Id",
+       o."Email",
+       o."GoogleSubId",
+       o."AccessLevel",
+       o."OfficerTitle",
+       o."IsEnabled",
+       o."MemberId",
+       o."CreatedAt"
+FROM public."OfficerAccounts" as "o"
+inner join public."Members" as "m"
+on "o"."MemberId" = "m"."Id"
+where m."Id"
+ in (
+47,
+23,
+52,
+84,
+37,
+109
+);
 
+SELECT 
+*
+FROM public."Members"
+where "Id"
+ in (
+47,
+23,
+52,
+84,
+37,
+109
+)
